616e17e7af2b5b10b2bba306a31e0f0b3d4298a2
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-12-21 14:56:23 +0300

    feat: background color can now be changed using socket

--- chat/client/src/App.js
+++ chat/client/src/App.js
@@ -1,17 +1,22 @@
 import Palette from "./components/Palette";
-import {initSocket} from './socketApi'
-import React from "react";
+import {initSocket, subscribe} from './socketApi'
+import {React, useState} from "react";
 import {useEffect} from "react";
 import './App.css'
 
 function App() {
+  const [activeColor,setActiveColor] = useState('#282c34');
+
 
   useEffect(()=>{
     initSocket();
+    subscribe((color)=>{
+      setActiveColor(color)
+    });
   },[]);
 
   return (
-    <div className="App">
+    <div className="App" style={{backgroundColor:activeColor}}>
       <Palette/>
     </div>
   );
