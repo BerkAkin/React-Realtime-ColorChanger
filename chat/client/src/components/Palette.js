2bcbff81b5cb4d2e926437f5f5eb4f772776cb55
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-12-21 14:45:20 +0300

    feat: client can now send data to backend

--- chat/client/src/components/Palette.js
+++ chat/client/src/components/Palette.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import {useState,React} from 'react'
+import { send } from '../socketApi'
+
 
 function Palette() {
+    const [color,setColor] = useState('#000000');
+
   return (
     <div className='palette'>
-        <input type='color'></input>
-        <button>Click</button>
+        <input type='color' value={color} onChange={(event)=>setColor(event.target.value)}></input>
+        <button onClick={()=>send(color)}>Click</button>
     </div>
   )
 }
