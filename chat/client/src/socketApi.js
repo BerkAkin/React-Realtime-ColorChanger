c5614d818ee4804cb6a56dc56eb3dab6a6dde504
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-12-21 14:38:18 +0300

    initialization of socket connection

---
+++ chat/client/src/socketApi.js
@@ -0,0 +1,16 @@
+import io from 'socket.io-client'
+
+let socket;
+
+export const initSocket = () => {
+
+    console.log("Sunucuya bağlanılıyor")
+
+    socket = io('http://localhost:3001',{
+        transports:["websocket"]
+    })
+
+    socket.on('connect',()=>{
+        console.log("Sunucuya Bağlanıldı")
+    })
+}
