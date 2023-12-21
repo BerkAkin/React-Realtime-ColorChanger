c46f0d6b1906ce56a8022ada56d433940001be14
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2023-12-21 14:38:00 +0300

    feat: socket.io usage

--- chat/client/src/App.js
+++ chat/client/src/App.js
@@ -1,8 +1,15 @@
 import Palette from "./components/Palette";
+import {initSocket} from './socketApi'
 import React from "react";
+import {useEffect} from "react";
 import './App.css'
 
 function App() {
+
+  useEffect(()=>{
+    initSocket();
+  },[]);
+
   return (
     <div className="App">
       <Palette/>
